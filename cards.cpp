1609d5473e330de01c8d8cd89072e8b4b1021dad
alepelayo <alepelayo@g.ucla.edu>
2018-05-07 21:01:07 -0700

    adding get_points() member function

--- cards.cpp
+++ cards.cpp
@@ -1,172 +1,167 @@
 #include "cards.h"
 #include <cstdlib>
 #include <iostream>
-
-/* 
-You might or might not need these two extra libraries 
 #include <iomanip>
-#include <algorithm>
-*/
+#include <vector>
 
 
 /* *************************************************
-   Card class
-   ************************************************* */
+Card class
+************************************************* */
 
-/* 
-   Default constructor for the Card class.
-   It could give repeated cards. This is OK.
-   Most variations of Blackjack are played with 
-   several decks of cards at the same time.
+/*
+Default constructor for the Card class.
+It could give repeated cards. This is OK.
+Most variations of Blackjack are played with
+several decks of cards at the same time.
 */
-Card::Card(){
-   int r = 1 + rand() % 4;
-   switch (r) {
-      case 1: suit = OROS; break;
-      case 2: suit = COPAS; break; 
-      case 3: suit = ESPADAS; break;
-      case 4: suit = BASTOS; break; 
-      default: break;
-   }
+Card::Card() {
+	int r = 1 + rand() % 4;
+	switch (r) {
+	case 1: suit = OROS; break;
+	case 2: suit = COPAS; break;
+	case 3: suit = ESPADAS; break;
+	case 4: suit = BASTOS; break;
+	default: break;
+	}
 
-   r = 1 + rand() % 10;  
-   switch (r) {
-      case  1: rank = AS; break; 
-      case  2: rank = DOS; break; 
-      case  3: rank = TRES; break; 
-      case  4: rank = CUATRO; break; 
-      case  5: rank = CINCO; break; 
-      case  6: rank = SEIS; break; 
-      case  7: rank = SIETE; break; 
-      case  8: rank = SOTA; break; 
-      case  9: rank = CABALLO; break; 
-      case 10: rank = REY; break; 
-      default: break;
-   }
+	r = 1 + rand() % 10;
+	switch (r) {
+	case  1: rank = AS; break;
+	case  2: rank = DOS; break;
+	case  3: rank = TRES; break;
+	case  4: rank = CUATRO; break;
+	case  5: rank = CINCO; break;
+	case  6: rank = SEIS; break;
+	case  7: rank = SIETE; break;
+	case  8: rank = SOTA; break;
+	case  9: rank = CABALLO; break;
+	case 10: rank = REY; break;
+	default: break;
+	}
 }
 
 // Accessor: returns a string with the suit of the card in Spanish 
-string Card::get_spanish_suit() const { 
-   string suitName;
-   switch (suit) {
-      case OROS: 
-         suitName = "oros"; 
-	 break;
-      case COPAS: 
-         suitName = "copas"; 
-	 break; 
-      case ESPADAS: 
-         suitName = "espadas"; 
-	 break;
-      case BASTOS: 
-         suitName = "bastos"; 
-	 break; 
-      default: break;
-   }
-   return suitName;
+string Card::get_spanish_suit() const {
+	string suitName;
+	switch (suit) {
+	case OROS:
+		suitName = "oros";
+		break;
+	case COPAS:
+		suitName = "copas";
+		break;
+	case ESPADAS:
+		suitName = "espadas";
+		break;
+	case BASTOS:
+		suitName = "bastos";
+		break;
+	default: break;
+	}
+	return suitName;
 }
 
 // Accessor: returns a string with the rank of the card in Spanish 
-string Card::get_spanish_rank() const { 
-   string rankName;
-   switch (rank) {
-      case AS:
-         rankName = "As"; 
-	 break; 
-      case DOS: 
-         rankName = "Dos"; 
-	 break; 
-      case TRES: 
-         rankName = "Tres"; 
-	 break; 
-      case CUATRO: 
-         rankName = "Cuatro"; 
-	 break; 
-      case CINCO: 
-         rankName = "Cinco"; 
-	 break; 
-      case SEIS: 
-         rankName = "Seis"; 
-	 break; 
-      case SIETE: 
-         rankName = "Siete"; 
-	 break; 
-      case SOTA: 
-         rankName = "Sota"; 
-	 break; 
-      case CABALLO: 
-         rankName = "Caballo"; 
-	 break; 
-      case REY: 
-         rankName = "Rey"; 
-	 break; 
-      default: break;
-   }
-   return rankName;
+string Card::get_spanish_rank() const {
+	string rankName;
+	switch (rank) {
+	case AS:
+		rankName = "As";
+		break;
+	case DOS:
+		rankName = "Dos";
+		break;
+	case TRES:
+		rankName = "Tres";
+		break;
+	case CUATRO:
+		rankName = "Cuatro";
+		break;
+	case CINCO:
+		rankName = "Cinco";
+		break;
+	case SEIS:
+		rankName = "Seis";
+		break;
+	case SIETE:
+		rankName = "Siete";
+		break;
+	case SOTA:
+		rankName = "Sota";
+		break;
+	case CABALLO:
+		rankName = "Caballo";
+		break;
+	case REY:
+		rankName = "Rey";
+		break;
+	default: break;
+	}
+	return rankName;
 }
-
 
 
 // Accessor: returns a string with the suit of the card in English 
 // This is just a stub! Modify it to your liking.
-string Card::get_english_suit() const { 
-      string suitName;
-   switch (suit) {
-      case OROS: 
-         suitName = "coins"; 
-    break;
-      case COPAS: 
-         suitName = "cups"; 
-    break; 
-      case ESPADAS: 
-         suitName = "spades"; 
-    break;
-      case BASTOS: 
-         suitName = "clubs"; 
-    break; 
-      default: break;
-   }
-   return suitName;;
+string Card::get_english_suit() const {
+	string suitName;
+	switch (suit) {
+	case OROS:
+		suitName = "coins";
+		break;
+	case COPAS:
+		suitName = "cups";
+		break;
+	case ESPADAS:
+		suitName = "spades";
+		break;
+	case BASTOS:
+		suitName = "clubs";
+		break;
+	default: break;
+	}
+	return suitName;;
 }
 
 // Accessor: returns a string with the rank of the card in English 
 // This is just a stub! Modify it to your liking.
-string Card::get_english_rank() const { 
-     string rankName;
-   switch (rank) {
-      case AS:
-         rankName = "Ace"; 
-    break; 
-      case DOS: 
-         rankName = "Two"; 
-    break; 
-      case TRES: 
-         rankName = "Three"; 
-    break; 
-      case CUATRO: 
-         rankName = "Four"; 
-    break; 
-      case CINCO: 
-         rankName = "Five"; 
-    break; 
-      case SEIS: 
-         rankName = "Six"; 
-    break; 
-      case SIETE: 
-         rankName = "Seven"; 
-    break; 
-      case SOTA: 
-         rankName = "Jack"; 
-    break; 
-      case CABALLO: 
-         rankName = "Knight"; 
-    break; 
-      case REY: 
-         rankName = "King"; 
-    break; 
-      default: break;
-   }
-   return rankName;;
+string Card::get_english_rank() const {
+	string rankName;
+	switch (rank) {
+	case AS:
+		rankName = "Ace";
+		break;
+	case DOS:
+		rankName = "Two";
+		break;
+	case TRES:
+		rankName = "Three";
+		break;
+	case CUATRO:
+		rankName = "Four";
+		break;
+	case CINCO:
+		rankName = "Five";
+		break;
+	case SEIS:
+		rankName = "Six";
+		break;
+	case SIETE:
+		rankName = "Seven";
+		break;
+	case SOTA:
+		rankName = "Jack";
+		break;
+	case CABALLO:
+		rankName = "Knight";
+		break;
+	case REY:
+		rankName = "King";
+		break;
+	default: break;
+	}
+	return rankName;;
 }
 
 
@@ -174,47 +169,67 @@ string Card::get_english_rank() const {
 // Assigns a numerical value to card based on rank.
 // AS=1, DOS=2, ..., SIETE=7, SOTA=10, CABALLO=11, REY=12
 int Card::get_rank() const {
-   return static_cast<int>(rank) + 1 ;
+	return static_cast<int>(rank) + 1;
 }
 
 // Comparison operator for cards
 // Returns TRUE if card1 < card2
 bool Card::operator < (Card card2) const {
-   return rank < card2.rank;
+	return rank < card2.rank;
 }
 
 
 
 /* *************************************************
-   Hand class
-   ************************************************* */
+Hand class
+************************************************* */
 // Implemente the member functions of the Hand class here.
+Hand::Hand() {
+	vector<Card> hand;
+}
 
+void Hand::add_card() { //adds a random card 
+	Card carta;
+	hand.push_back(carta);
+}
+
+void Hand::display() {
+	for (size_t i = 0; i < hand.size(); ++i) {
+
+		cout << "\t" << hand[i].get_spanish_rank() << " de " << hand[i].get_spanish_suit();
+		cout << "\t" << right << "(" << hand[i].get_english_rank() << " of " << hand[i].get_english_suit() << ")." << endl;
+
+	}
+}
 
 
 /* *************************************************
-   Player class
-   ************************************************* */
+Player class
+************************************************* */
 // Implemente the member functions of the Player class here.
-Player::Player(int m) {
-   money = m;
-}
 
-int::Player get_money() const {
-   return money;
-}
 
-double ranktopoints(Card card) {
-   if(card.get_rank() > 7) {
-      points = 0.5;
-      return points;
-   }
-   else {
-      return card.get_rank();
-   }
-}
+//double Hand::ranktopoints() {
+//	Card carta; //this is a different card from the add_card member function
+//	if (carta.get_rank() > 7) {
+//		double points = 0.5;
+//		return points;
+//	}
+//	else {
+//		return carta.get_rank();
+//	}
+//}
 
-void display(Card card1) {
-   cout << setw(8) << card1.get_spanish_rank() << " de " << card1.get_spanish_suit();
-   cout << setw(8) << right << "(" << card1.get_english_rank() << " of " << card1.get_english_suit() << ")." << endl;
+double Hand::get_points() {
+	points = 0;
+	for (size_t i = 0; i < hand.size(); ++i) {
+
+		if (hand[i].get_rank() > 7) {
+			points += 0.5;
+		}
+		else {
+			points += hand[i].get_rank();
+		}
+	}
+	return points;
 }
