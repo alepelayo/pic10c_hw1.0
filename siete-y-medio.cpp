a18d917b03c8580988655a2ed36b9073a59ba4eb
alepelayo <alepelayo@g.ucla.edu>
2018-05-27 16:01:54 -0700

    fixing loops

--- siete-y-medio.cpp
+++ siete-y-medio.cpp
@@ -10,57 +10,125 @@ using namespace std;
 int main() {
 
 	srand((int)time(0));
-	int const STARTING_BALANCE = 100;
-	double current_points = STARTING_BALANCE;
+	int starting_balance = 100;
 
 	Hand player_uno;
 	Hand dealer;
 
-	Player player1 = Player(STARTING_BALANCE);
+	Player player1(player_uno, starting_balance);
+	Player dealer1(dealer, 900);
+
 	//if (player1.get_money() != 0 || player1.get_money() != 900) {
-	int bet;
+	size_t bet;
 	char decision;
 	cout << "You have $" << player1.get_money() << ". Enter bet: ";
 	cin >> bet;
-	if (bet > STARTING_BALANCE) {
+	if (bet > starting_balance) {
 		std::cout << "Your bet exceeds your starting balance. Please lower your bet: ";
 		cin >> bet;
 	}
 
-	player_uno.add_card();
+	//player_uno.add_card();
 
-	cout << "Your cards: " << endl;
-	player_uno.display();
+	//cout << "Your cards: " << endl;
+	//player_uno.display();
 
-	do {
-		std::cout << "Your total is " << player_uno.get_points() << ". Do you want another card (y/n)? ";
+	//cout << "Your total is " << player_uno.get_points() << ". Do you want another card (y/n)? ";
+	//cin >> decision;
+
+	decision = 'y';
+
+
+	while (decision == 'y') {
+		Card carta;
+		cout << "New card:" << endl;
+		display(carta);
+
+		cout << "Your cards:" << endl << endl;
+		player_uno.add_card(carta);
+		player_uno.displayAll();
+
+		cout << "Your total is " << player_uno.get_points() << ". Do you want another card (y/n)? ";
 		cin >> decision;
+	}
 
-		if (decision == 'y') { //player1 plays all cards before AI
+	while (dealer.get_points() < 6) {
+		Card carta;
+		dealer.add_card(carta);
+		dealer.displayAll();
+	}
 
-			cout << "New card: " << endl;
 
-			player_uno.add_card();
+	//do {
+	//	std::cout << "Your total is " << player_uno.get_points() << ". Do you want another card (y/n)? ";
+	//	cin >> decision;
+
+	//	if (decision == 'y') { //player1 plays all cards before AI
+
+	//		cout << "New card: " << endl;
+
+	//		player_uno.add_card(); //adding a new card
 
 
-			cout << "Your cards: " << endl;
-			player_uno.display();
+	//		cout << "Your cards: " << endl;
+	//		player_uno.display();
 
-			cout << player_uno.get_points() << endl;
-		}
+	//		cout << player_uno.get_points() << endl;
+	//	}
 
-		if (decision == 'n') {
+	//	if (decision == 'n') {
 
-			cout << "Dealer's cards: " << endl;
-			dealer.add_card();
+	//		cout << "Dealer's cards: " << endl;
+	//		dealer.add_card();
 
-			dealer.display();
+	//		dealer.display();
 
-			cout << dealer.get_points() << endl;
-		}
+	//		cout << dealer.get_points() << endl;
+	//	}
 
-	} while (dealer.get_points() < 6);
-	//while the dealer's hand does not exceed 7 points, they will draw another card
+	//		} while (dealer.get_points() < 6);
+	//		//while the dealer's hand does not exceed 7 points, they will draw another card
+
+
+
+
+
+
+	//size_t player::calcmoney(player, hand dealer) {
+	if (player_uno.get_points() > 7.5 && dealer.get_points() <= 7.5) {
+		cout << "you lose $ " << bet << ". " << endl;
+
+		//temporary ints for both player and dealer money
+		int player_temp = player1.get_money();
+		int dealer_temp = dealer1.get_money();
+
+		//changing money amounts for both dealer and player
+		player_temp -= bet;
+		dealer_temp += bet;
+
+		//setting the new money to private variable
+		player1.set_money(player_temp);
+		dealer1.set_money(dealer_temp);
+
+	}
+	else if (player_uno.get_points() <= 7.5 && dealer.get_points() > 7.5) {
+		cout << "you win $ " << bet << ". " << endl;
+
+		//temporary ints for both player and dealer money
+		int player_temp = player1.get_money();
+		int dealer_temp = dealer1.get_money();
+
+		//changing money amounts for both dealer and player
+		player_temp += bet;
+		dealer_temp -= bet;
+
+		//setting the new money to private variable
+		player1.set_money(player_temp);
+		dealer1.set_money(dealer_temp);
+
+	}
+	//}
+
 
 	cin.get();
 	cin.get();
