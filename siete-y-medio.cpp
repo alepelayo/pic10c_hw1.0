88f3d0da9516a167d0a427ead458c0398ed12c67
alepelayo <alepelayo@g.ucla.edu>
2018-05-27 20:24:58 -0700

    fixed the distribution of money based on wins/loses

--- siete-y-medio.cpp
+++ siete-y-medio.cpp
@@ -28,90 +28,49 @@ int main() {
 		cin >> bet;
 	}
 
-	//player_uno.add_card();
+	Card carta;
 
-	//cout << "Your cards: " << endl;
-	//player_uno.display();
+	cout << "Your cards:" << endl;
+	player_uno.add_card(carta);
+	player_uno.displayAll();
 
-	//cout << "Your total is " << player_uno.get_points() << ". Do you want another card (y/n)? ";
-	//cin >> decision;
-
-	decision = 'y';
-
+	cout << "Your total is " << player_uno.get_points() << ". Do you want another card (y/n)? ";
+	cin >> decision;
+	cout << endl;
 
 	while (decision == 'y') {
 		Card carta;
 		cout << "New card:" << endl;
 		display(carta);
 
-		cout << "Your cards:" << endl << endl;
+		cout << "Your cards:" << endl;
 		player_uno.add_card(carta);
 		player_uno.displayAll();
 
 		cout << "Your total is " << player_uno.get_points() << ". Do you want another card (y/n)? ";
 		cin >> decision;
+		cout << endl;
 	}
 
+	cout << "Dealer's cards: " << endl;
+	Card carta2;
+	dealer.add_card(carta2);
+	display(carta2);
+
 	while (dealer.get_points() < 6) {
-		Card carta;
-		dealer.add_card(carta);
+		cout << "New Card:" << endl;
+		Card carta3;
+		dealer.add_card(carta3);
 		dealer.displayAll();
 	}
 
-
-	//do {
-	//	std::cout << "Your total is " << player_uno.get_points() << ". Do you want another card (y/n)? ";
-	//	cin >> decision;
-
-	//	if (decision == 'y') { //player1 plays all cards before AI
-
-	//		cout << "New card: " << endl;
-
-	//		player_uno.add_card(); //adding a new card
+	cout << "The dealer's total is " << dealer.get_points() << endl;
 
 
-	//		cout << "Your cards: " << endl;
-	//		player_uno.display();
+	//Player wins and Dealer busts
+	if ((player_uno.get_points() > dealer.get_points() && player_uno.get_points() <= 7.5) ||
+		(dealer.get_points() > 7.5 && player_uno.get_points() <= 7.5)) {
 
-	//		cout << player_uno.get_points() << endl;
-	//	}
-
-	//	if (decision == 'n') {
-
-	//		cout << "Dealer's cards: " << endl;
-	//		dealer.add_card();
-
-	//		dealer.display();
-
-	//		cout << dealer.get_points() << endl;
-	//	}
-
-	//		} while (dealer.get_points() < 6);
-	//		//while the dealer's hand does not exceed 7 points, they will draw another card
-
-
-
-
-
-
-	//size_t player::calcmoney(player, hand dealer) {
-	if (player_uno.get_points() > 7.5 && dealer.get_points() <= 7.5) {
-		cout << "you lose $ " << bet << ". " << endl;
-
-		//temporary ints for both player and dealer money
-		int player_temp = player1.get_money();
-		int dealer_temp = dealer1.get_money();
-
-		//changing money amounts for both dealer and player
-		player_temp -= bet;
-		dealer_temp += bet;
-
-		//setting the new money to private variable
-		player1.set_money(player_temp);
-		dealer1.set_money(dealer_temp);
-
-	}
-	else if (player_uno.get_points() <= 7.5 && dealer.get_points() > 7.5) {
 		cout << "you win $ " << bet << ". " << endl;
 
 		//temporary ints for both player and dealer money
@@ -127,12 +86,49 @@ int main() {
 		dealer1.set_money(dealer_temp);
 
 	}
-	//}
 
+	//Player busts and Dealer wins
+	else if ((dealer.get_points() > player_uno.get_points() && dealer.get_points() <= 7.5) ||
+		(dealer.get_points() > 7.5 && player_uno.get_points() <= 7.5)) {
+
+		cout << "you lose $ " << bet << ". " << endl;
+
+		//temporary ints for both player and dealer money
+		int player_temp = player1.get_money();
+		int dealer_temp = dealer1.get_money();
+
+		//changing money amounts for both dealer and player
+		player_temp -= bet;
+		dealer_temp += bet;
+
+		//setting the new money to private variable
+		player1.set_money(player_temp);
+		dealer1.set_money(dealer_temp);
+
+	}
+
+	//Both Player and Dealer Bust
+	else if (player_uno.get_points() > 7.5 && dealer.get_points() > 7.5) {
+		cout << "you lose $ " << bet << ". " << endl;
+
+		//temporary ints for both player and dealer money
+		int player_temp = player1.get_money();
+		int dealer_temp = dealer1.get_money();
+
+		//changing money amounts for both dealer and player
+		player_temp -= bet;
+		dealer_temp -= bet;
+
+		//setting the new money to private variable
+		player1.set_money(player_temp);
+		dealer1.set_money(dealer_temp);
+	}
+
+	else if (dealer.get_points() == player_uno.get_points()) {
+		cout << "There has been a tie. No money will be won or lost." << endl;
+	}
 
 	cin.get();
 	cin.get();
 	return 0;
 }
-
-
