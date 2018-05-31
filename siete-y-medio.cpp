1ac1085a33c5dfc4379895f98b8ce48892b3bb07
Alejandra Uribe <alepelayo@pic.ucla.edu>
2018-05-31 10:05:26 -0700

    fixing house advantage

--- siete-y-medio.cpp
+++ siete-y-medio.cpp
@@ -8,12 +8,14 @@
 
 using namespace std;
 
+//just want to check if it notices any changes with this comment
+
 int main() {
 
 	//creating a gamelog.txt which keeps track of each round, including betting value and hands for both dealer and input user
 	ofstream gamelog;
 	gamelog.open("gamelog.txt");
-	
+
 	srand((int)time(0)); //Random number, or in our case, card generator
 
 	int starting_balance_player = 100; //starting balance for player/input user
@@ -21,8 +23,8 @@ int main() {
 
 	int gameNum = 0; //keeps count of how many rounds are played for the gamelog.txt
 
-	//creating a playing hand for both input user and dealer
-	Hand player_uno; 
+					 //creating a playing hand for both input user and dealer
+	Hand player_uno;
 	Hand dealer;
 
 	//creating the player themselves, which includes their hand and money amount
@@ -35,7 +37,7 @@ int main() {
 		char decision; //input decision on whether the player wishes to add another card to their deck
 		++gameNum; //keeps track of how many rounds are being played
 
-		//the initial start of a round: input user is being asked how much money they wish to bet
+				   //the initial start of a round: input user is being asked how much money they wish to bet
 		cout << "You have $" << player1.get_money() << ". Enter bet: ";
 		cin >> bet;
 		if (bet > starting_balance_player) { //making sure that the bet number is valid to play
@@ -50,7 +52,7 @@ int main() {
 
 		Card carta; //creating the player's first card
 
-		//outputting the player's first card
+					//outputting the player's first card
 		cout << "Your cards:" << endl;
 		player_uno.add_card(carta);
 		player_uno.displayAll();
@@ -75,7 +77,7 @@ int main() {
 			player_uno.add_card(carta); //adding the specific card into the player's hand
 			player_uno.displayAll(); //displaying all current cards in player's hand
 
-			//outputting the player's current point count and asks if they wish to receive another card
+									 //outputting the player's current point count and asks if they wish to receive another card
 			cout << "Your total is " << player_uno.get_points() << ". Do you want another card (y/n)? ";
 			cin >> decision;
 			cout << endl;
@@ -96,7 +98,7 @@ int main() {
 		display(carta2); //displaying the specific card
 
 		if (dealer.get_points() >= 6) {
-			cout << "The dealer's total is " << dealer.get_points() << "." << endl << endl; 
+			cout << "The dealer's total is " << dealer.get_points() << "." << endl << endl;
 			//still trying to figure out how to only show one dealer total
 		}
 
@@ -167,21 +169,18 @@ int main() {
 
 		}
 
-		//Both player and dealer busts
+		//Both player and dealer busts. Only the player loses money (house advantage)
 		else if (player_uno.get_points() > 7.5 && dealer.get_points() > 7.5) {
 			cout << "Both you and the dealer have bust. You both lose $" << bet << ". " << endl << endl;
 
 			//temporary ints for both player and dealer money
 			int player_temp = player1.get_money();
-			int dealer_temp = dealer1.get_money();
 
 			//changing money amounts for both dealer and player
 			player_temp -= bet;
-			dealer_temp -= bet;
 
 			//setting the new money to private variable
 			player1.set_money(player_temp);
-			dealer1.set_money(dealer_temp);
 		}
 
 		//if both the dealer's and player's points are tied in that specific round
@@ -205,7 +204,7 @@ int main() {
 		starting_balance_dealer = dealer1.get_money();
 
 		//clearing both the player and dealer's hands in order to start a new round
-		player_uno.clear(); 
+		player_uno.clear();
 		dealer.clear();
 
 	}
